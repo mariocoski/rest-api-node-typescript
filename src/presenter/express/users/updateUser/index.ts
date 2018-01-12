14da81bb9231737de92af6d4af757ba3163bbf65
Mariocoski <mariuszrajczakowski@gmail.com>
2018-01-12 20:17:09 +0000

    added response for updateUser

--- src/presenter/express/users/updateUser/index.ts
+++ src/presenter/express/users/updateUser/index.ts
@@ -8,13 +8,6 @@ import {PERMISSION_UPDATE_USER} from '../../../../utils/constants';
 import {maybe, optional,checkType,composeRules, first, restrictToSchema}from 'rulr';
 import {ModelNotFoundError} from '../../../../utils/errors';
 
-// const validateGetUsers = maybe(
-//   restrictToSchema({
-//     limit: optional(checkType(Number)),
-//     offset: optional(checkType(Number)),
-//     order: optional(checkType(String))
-//   }),
-// );
 
 export default (config: Config) => {
   return catchErrors(config, async (req: Request, res: Response): Promise<void> => {
@@ -23,6 +16,7 @@ export default (config: Config) => {
 
     hasPermission({user: authenticatedUser, permissionName: PERMISSION_UPDATE_USER});
 
+    res.status(OK_200_HTTP_CODE).json({});
   });
 
 };
