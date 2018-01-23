05939731fbc5021447916b45797e7ad290b44433
Mariocoski <mariuszrajczakowski@gmail.com>
2018-01-23 21:06:06 +0000

    added regExp for isSortValid function

--- src/utils/validate/validate.test.ts
+++ src/utils/validate/validate.test.ts
@@ -3,9 +3,22 @@ describe(__filename, () => {
 
   it('should return false when sort string is invalid', () =>{
     const notValid = "id:desc,ageasc:";
-    const notValid2 = "";
+    const notValid2 = "created_at:asc,,id:desc";
+    const notValid3 = "";
+    const notValid4 = "created_at:asc,id:desc,,";
     expect(isSortValid(notValid)).toBe(false);
     expect(isSortValid(notValid2)).toBe(false);
+    expect(isSortValid(notValid3)).toBe(false);
+    expect(isSortValid(notValid4)).toBe(false);
+  });
+
+  it('should return true when sort string is valid', () =>{
+    const valid = "id:desc";
+    const valid2 = "created_at:asc,id:desc";
+    const valid3 = "created_at:asc,id:desc,";
+    expect(isSortValid(valid)).toBe(true);
+    expect(isSortValid(valid2)).toBe(true);
+    expect(isSortValid(valid3)).toBe(true);
   });
 
 });
