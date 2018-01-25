09ae4b922c11affbf14aa71c13aeb835df9fe0ec
Mariocoski <mariuszrajczakowski@gmail.com>
2018-01-25 20:38:17 +0000

    added functionality to createOrderObject

--- src/utils/createOrderObject.test.ts
+++ src/utils/createOrderObject.test.ts
@@ -2,7 +2,16 @@ import createOrderObject from './createOrderObject';
 
 describe(__filename, () => {
   it('should transform valid sort string to valid order object', () =>{
-    const validSortString = "created_at:asc,id:desc";
-    expect(createOrderObject(validSortString)).toEqual([['created_at','asc'],['id','desc']]);
+    const validSortString = "created_at:asc,id:desc,another_field:asc";
+    expect(createOrderObject(validSortString)).toEqual([
+      ['created_at','asc'],['id','desc'], ['another_field','asc']
+    ]);
+  });
+
+  it('should transform valid sort string to valid order object only for provided attributes', () =>{
+    const validSortString = "created_at:asc,id:desc,another_field:asc";
+    expect(createOrderObject(validSortString, ['created_at','id'])).toEqual([
+      ['created_at','asc'],['id','desc']
+    ]);
   });
 });
--- src/utils/createOrderObject.ts
+++ src/utils/createOrderObject.ts
@@ -1,6 +1,7 @@
-
-
-export default (sortString: string, pickableAttributes: string[] = []) => {
-  
-  return [];
+export default (sortString: string, pickableAttributes: string[] = []): string[][] => {
+  return sortString.toLowerCase().split(',').reduce((prev: any, curr: any)=>{
+    const [key, value] = curr.split(':');
+    const item = pickableAttributes.length ? (pickableAttributes.indexOf(key) !== -1 ? [[key, value]]: [] ): [[key, value]] ; 
+    return prev.concat(item);
+  },[]);
 }
