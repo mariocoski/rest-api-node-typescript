a615685a70cf6f8d292211f9a095a2ed15989c03
Mariusz Rajczakowski <mariuszrajczakowski@gmail.com>
2018-01-26 11:02:59 +0000

    Update createOrderObject.ts

--- src/utils/createOrderObject.ts
+++ src/utils/createOrderObject.ts
@@ -1,7 +1,9 @@
 export default (sortString: string, pickableAttributes: string[] = []): string[][] => {
   return sortString.toLowerCase().split(',').reduce((prev: any, curr: any)=>{
     const [key, value] = curr.split(':');
-    const item = pickableAttributes.length ? (pickableAttributes.indexOf(key) !== -1 ? [[key, value]]: [] ): [[key, value]] ; 
-    return prev.concat(item);
+    if (pickableAttributes.indexOf(key) !== -1) {
+      return [...prev, [key, value]];
+    }
+    return prev;
   },[]);
 }
